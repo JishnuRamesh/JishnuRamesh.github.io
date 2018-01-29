3d88199ac4b834db325e2b2930a406fa11b84fd6
Jishnu Ramesh <Jish_r@live.com>
2018-01-29 11:38:10 +1100

    Updated navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+function navScroll(){
+  var myNav = document.getElementById("navigation");
+  if (document.documentElement.scrollTop > 550 || document.body.scrollTop > 550) {
+    myNav.classList.add("nav-colored");
+  }
+  else if (myNav.classList.contains("nav-colored")) {
+    myNav.classList.remove("nav-colored");
+  }
+}
 
+
+
+
+
+
+
+window.onscroll = function() {
+  navScroll();
+};
